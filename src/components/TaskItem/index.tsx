502e55cbcaf130ffe0982ee0ceff6967f7943795
manojmohangit <manoj.mohana1011@gmail.com>
2026-02-20 06:09:42 +0530

    Improvement: Added animation on deletion fo item.

--- src/components/TaskItem/index.tsx
+++ src/components/TaskItem/index.tsx
@@ -3,12 +3,27 @@ import { NavLink } from "react-router";
 import './index.scss';
 import { useTaskContext } from "../../TaskStorageContext";
 import { formatDate } from "../../utils/formatDate";
+import { useState } from "react";
 
 function TaskItem({ task }:{ task : Task }) {
     const { deleteTask } = useTaskContext();
+    const [isDeleting, setIsDeleting] = useState(false);
+
+
+    function startDeleteAnimation() {
+        setIsDeleting(true);
+    }
 
     return (
-        <li className={`task-card ${task.status}`}>
+        <li 
+            className={`task-card ${task.status} ${isDeleting ? 'deleting' : ''}`} 
+            onAnimationEnd={
+                () => {
+                    if(isDeleting) 
+                        deleteTask(task.id);
+                }
+            }
+        >
             <div className="card-icon">{task.title.charAt(0)}</div>
             <div className='card-content'>
                 <div className="card-header">
@@ -24,7 +39,7 @@ function TaskItem({ task }:{ task : Task }) {
                     <span>{formatDate(task.id)}</span>
                     <div className="task-actions">
                         <NavLink to={`/edit/${task.id}`} viewTransition className="text-primary"><i className="bi bi-pencil"></i></NavLink>
-                        <button className="delete-btn" onClick={() => { deleteTask(task.id); }}><i className="bi bi-trash text-red"></i></button>
+                        <button className="delete-btn" onClick={startDeleteAnimation}><i className="bi bi-trash text-red"></i></button>
                     </div>
                     
                 </div>
